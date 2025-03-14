a59c9c6d9fc9df93f0ffc493b76de0d967c14e5b
OhJeongHun <45549589+ojh6507@users.noreply.github.com>
2025-03-14 14:31:49 +0900

    Update PixelTextureShader.hlsl

--- 2Dto3D/Week0v2/PixelTextureShader.hlsl
+++ 2Dto3D/Week0v2/PixelTextureShader.hlsl
@@ -8,5 +8,10 @@ struct PSInput {
 
 float4 main(PSInput input) : SV_TARGET {
 
-    return gTexture.Sample(gSampler, input.texCoord);
+    float4 col = gTexture.Sample(gSampler, input.texCoord);
+    float threshold = 0.05; // 필요한 경우 임계값을 조정
+    if (col.r < threshold && col.g < threshold && col.b < threshold)
+        clip(-1); // 픽셀 버리기
+    
+    return col;
 }
