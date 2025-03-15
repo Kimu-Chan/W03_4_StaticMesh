88a8fbf5d237302ce189a7e722810c8e59a66d2d
Jay <seojw9292@gmail.com>
2025-03-15 16:58:37 +0900

    resolve conflict

--- 2Dto3D/Week0v2/ShowFlags.h
+++ 2Dto3D/Week0v2/ShowFlags.h
@@ -1,7 +1,36 @@
 #pragma once
 #include "Define.h"
+#include <Windows.h>
+#include "IWindowToggleable.h"
+
+#include <cstdint>
+
+class UWorld;
+class ShowFlags : public IWindowToggleable
+{
+private:
+	ShowFlags();
+
+public:
+	~ShowFlags();
+
+	static ShowFlags& GetInstance();
+
+	void Draw(UWorld& world);
+	void OnResize(HWND hWnd);
+	void Toggle() override {
+		if (bWasOpen) {
+			bWasOpen = false;
+		}
+	}
+private:
+	bool bWasOpen = true;
+	UINT width;
+	UINT height;
+};
+
 enum class EEngineShowFlags : uint64
 {
-    SF_Primitives,
-    SF_BillboardText,
+	SF_Primitives,
+	SF_BillboardText,
 };
