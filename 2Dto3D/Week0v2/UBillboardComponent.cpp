fcb70261f04fac011c3f3fe61e57033251d82863
kbjun98 <110660221+kbjun98@users.noreply.github.com>
2025-03-16 23:40:47 +0900

    피킹 버그 수정

--- 2Dto3D/Week0v2/UBillboardComponent.cpp
+++ 2Dto3D/Week0v2/UBillboardComponent.cpp
@@ -62,6 +62,48 @@ void UBillboardComponent::Render()
 	FEngineLoop::renderer.PrepareShader();
 }
 
+
+int UBillboardComponent::CheckRayIntersection(FVector& rayOrigin, FVector& rayDirection, float& pfNearHitDistance)
+{
+
+	/*
+	FMatrix worldMatrix = CreateBillboardMatrix();
+	FMatrix ViewMatrix = GEngineLoop.View;
+	FMatrix inverseMatrix = FMatrix::Inverse(worldMatrix * ViewMatrix);
+
+	FVector cameraOrigin = { 0,0,0 };
+	FVector pickRayOrigin = inverseMatrix.TransformPosition(cameraOrigin);
+
+
+	POINT mousePos;
+	GetCursorPos(&mousePos);
+	ScreenToClient(GEngineLoop.hWnd, &mousePos);
+
+	D3D11_VIEWPORT viewport;
+	UINT numViewports = 1;
+	FEngineLoop::graphicDevice.DeviceContext->RSGetViewports(&numViewports, &viewport);
+	float screenWidth = viewport.Width;
+	float screenHeight = viewport.Height;
+
+	FVector pickPosition;
+	int screenX = mousePos.x;
+	int screenY = mousePos.y;
+	FMatrix projectionMatrix = GetEngine().Projection;
+	pickPosition.x = ((2.0f * screenX / viewport.Width) - 1) / projectionMatrix[0][0];
+	pickPosition.y = -((2.0f * screenY / viewport.Height) - 1) / projectionMatrix[1][1];
+	pickPosition.z = 1.0f; // Near Plane
+
+	FVector _rayDirection = inverseMatrix.TransformPosition(pickPosition);
+	_rayDirection = (_rayDirection - pickRayOrigin).Normalize(); // local 좌표축의 ray
+
+	return Super::CheckRayIntersection(pickRayOrigin, _rayDirection, pfNearHitDistance);
+	*/
+	return CheckPickingOnNDC();
+}
+
+
+
+
 void UBillboardComponent::SetTexture(FWString _fileName)
 {
 	m_texture.init(_fileName);
@@ -112,4 +154,69 @@ void UBillboardComponent::CreateQuadTextureVertexBuffer()
 	if (!indexTextureBuffer) {
 		Console::GetInstance().AddLog(LogLevel::Warning, "Buffer Error");
 	}
+}
+
+bool UBillboardComponent::CheckPickingOnNDC()
+{
+	bool result = false;
+	POINT mousePos;
+	GetCursorPos(&mousePos);
+	ScreenToClient(GEngineLoop.hWnd, &mousePos);
+
+	D3D11_VIEWPORT viewport;
+	UINT numViewports = 1;
+	FEngineLoop::graphicDevice.DeviceContext->RSGetViewports(&numViewports, &viewport);
+	float screenWidth = viewport.Width;
+	float screenHeight = viewport.Height;
+
+	FVector pickPosition;
+	int screenX = mousePos.x;
+	int screenY = mousePos.y;
+	FMatrix projectionMatrix = GetEngine().Projection;
+	pickPosition.x = ((2.0f * screenX / viewport.Width) - 1);
+	pickPosition.y = -((2.0f * screenY / viewport.Height) - 1);
+	//pickPosition.y = ((2.0f * (viewport.Height - screenY) / viewport.Height) - 1);
+	pickPosition.z = 1.0f; // Near Plane
+
+	FMatrix M = CreateBillboardMatrix();
+	FMatrix V = GEngineLoop.View;
+	FMatrix P = projectionMatrix;
+	FMatrix MVP = M * V * P;
+
+	FVector4 transformedVerts[4];
+	//TODO 하드코딩
+	for (int i = 0; i < 4; i++)
+	{
+		FVector4 v = FVector4(quadTextureVertices[i].x, quadTextureVertices[i].y, quadTextureVertices[i].z, 1.0f);
+		FVector4 clipPos = FMatrix::TransformVector(v, MVP);
+		UE_LOG(LogLevel::Display, "ClipPos %f, %f, %f, %f", clipPos.x, clipPos.y, clipPos.z, clipPos.a);
+
+		if (clipPos.a != 0)
+			transformedVerts[i] = clipPos / clipPos.a;
+		else
+			transformedVerts[i] = clipPos;
+	}
+	float yCorrection = 1.0f / transformedVerts[0].a;
+	float minX = min(min(transformedVerts[0].x, transformedVerts[1].x), min(transformedVerts[2].x, transformedVerts[3].x));
+	float maxX = max(max(transformedVerts[0].x, transformedVerts[1].x), max(transformedVerts[2].x, transformedVerts[3].x));
+	float minY = min(min(transformedVerts[0].y, transformedVerts[1].y), min(transformedVerts[2].y, transformedVerts[3].y));
+	float maxY = max(max(transformedVerts[0].y, transformedVerts[1].y), max(transformedVerts[2].y, transformedVerts[3].y));
+
+	//minY *= yCorrection;
+	//maxY *= yCorrection;
+
+	for (int i = 0; i < 4; i++)
+	{
+		UE_LOG(LogLevel::Display, "Quad %f, %f, %f, %f",
+		transformedVerts[i].x, transformedVerts[i].y, transformedVerts[i].z, transformedVerts[i].a);
+	}
+	UE_LOG(LogLevel::Display, "My Click %f, %f, %f", pickPosition.x, pickPosition.y, pickPosition.z);
+	
+	if (pickPosition.x >= minX && pickPosition.x <= maxX &&
+		pickPosition.y >= minY && pickPosition.y <= maxY)
+	{
+		result = true;
+	}
+
+	return result;
 }
--- 2Dto3D/Week0v2/UBillboardComponent.h
+++ 2Dto3D/Week0v2/UBillboardComponent.h
@@ -14,6 +14,9 @@ public:
 	virtual void		Update(double deltaTime)	override;
 	virtual void		Release()					override;
 	virtual void		Render()					override;
+	virtual int			CheckRayIntersection(FVector& rayOrigin, 
+		FVector& rayDirection, float& pfNearHitDistance);
+
 	void SetTexture(FWString _fileName);
 protected:
 	UTexture m_texture;
@@ -24,9 +27,12 @@ protected:
 	UINT numVertices;
 	UINT numIndices;
 
+	float width;
+	float height;
+
 private:
 	void CreateQuadTextureVertexBuffer();
-
+	bool CheckPickingOnNDC();
 
 
 };
